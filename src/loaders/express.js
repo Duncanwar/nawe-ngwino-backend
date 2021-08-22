ce6a4e294b98914552d1556edc3b671de49c7c5c
Duncanwar <semunda9@gmail.com>
2021-08-22 23:14:07 +0200

    placing in a good place

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,8 +1,6 @@
 import cors from "cors";
-import middleroute from "./middleroute";
 
 export default async (app) => {
   app.use(cors());
-  middleroute(app);
   return app;
 };
--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,5 +1,7 @@
 import expressLoader from "./express";
+import fallbackRoute from "./fallbackroute";
 
 export default async (expressApp) => {
   expressLoader(expressApp);
+  fallbackRoute(expressApp);
 };
